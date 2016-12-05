e58aafff3b7618514eda225ff09031c490779f14
Duke <duke00360753@gmail.com>
2016-12-05 19:22:18 +0800

    1.完成excel 匯入相關設定 篇章 竹簡 字 2.Model關聯修正

---
+++ resources/views/frontend/excelImport.blade.php
@@ -0,0 +1,37 @@
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+ <title>Import  Laravel </title>
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" >
+</head>
+<body>
+ <nav class="navbar navbar-default">
+  <div class="container-fluid">
+   <div class="navbar-header">
+    <a class="navbar-brand" href="#"> Import Excel or CSV into  Database</a>
+   </div>
+  </div>
+ </nav>
+ <div class="container">
+  <form style="border: 4px solid #a1a1a1;margin-top: 15px;padding: 10px;" action="{{ URL::to('importArticle') }}" class="form-horizontal" method="post" enctype="multipart/form-data" >
+   <input type="file" name="import_file" />
+
+                 {{ csrf_field() }}
+   <button class="btn btn-primary">篇章</button>
+  </form>
+  <form style="border: 4px solid #a1a1a1;margin-top: 15px;padding: 10px;" action="{{ URL::to('importSlip') }}" class="form-horizontal" method="post" enctype="multipart/form-data" >
+   <input type="file" name="import_file" />
+
+                 {{ csrf_field() }}
+   <button class="btn btn-primary">竹簡</button>
+  </form>
+  <form style="border: 4px solid #a1a1a1;margin-top: 15px;padding: 10px;" action="{{ URL::to('importCharacter') }}" class="form-horizontal" method="post" enctype="multipart/form-data" >
+   <input type="file" name="import_file" />
+
+                 {{ csrf_field() }}
+   <button class="btn btn-primary">字</button>
+  </form>
+ </div>
+</body>
+</html>
