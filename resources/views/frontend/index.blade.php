6fcb9d1e24f8624057fbc7ae644fcd2d338f4da3
Duke <duke00360753@gmail.com>
2016-12-16 15:54:56 +0800

    1.add font face

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -2,6 +2,20 @@
 <head>
   {!! Html::style(url('css/app.css'))!!}
   {!! Html::script(url('js/app.js'))!!}
+  <style>
+
+        @font-face {
+            font-family: 'shigon';
+            src: url('fonts/chu6.tte'), url('fonts/chucdpeudpn6.tbl'), url('fonts/chueudccjn6.tbl')
+        }
+        .title {} body {
+            font-family: 'shigon';
+        }
+        #text {
+            font-family: 'shigon';
+        }
+
+    </style>
 </head>
 <body>
   <div class="container">
